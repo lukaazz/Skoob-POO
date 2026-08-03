babd7a83901698f6e01a428b68016e695da3769f
lukaazz <lucas009.lopes@gmail.com>
2026-08-03 11:25:32 -0300

    Classe Livro

---
+++ src/modelo/livro/Livro.java
@@ -0,0 +1,51 @@
+// Livro.java
+
+abstract class Livro {
+
+    protected String titulo;
+    protected String autor; 
+    protected String sinopse;
+    protected int id;
+
+
+    public Livro(String titulo, String autor, String sinopse, int id) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.sinopse = sinopse;
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    } 
+
+
+}
