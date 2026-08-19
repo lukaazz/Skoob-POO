78558b4e9f21512c75a0663e2a1af3638ad2f9b1
anthonelacr <anthonelacribeiro@gmail.com>
2026-08-19 11:27:16 -0300

    administrador

---
+++ src/modelo/usuario/Administrador.java
@@ -0,0 +1,14 @@
+public class Administrador extends Usuario {
+
+    public Administrador(String nome, String email, String senha) {
+        super(nome, email, senha);
+    }
+
+    public void removerLivroCatalogo() {
+        /* Implementação para remover livro do catálogo */
+    }
+
+    public void adicionarLivroCatalogo() {
+        /* Implementação para adicionar livro ao catálogo */
+    }
+}
