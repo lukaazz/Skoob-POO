2f6ae01e00f9c56a8dbf46e1e1c5b01f233a3687
lukaazz <lucas009.lopes@gmail.com>
2026-08-19 21:33:03 -0300

    criação da classe cadastro e do catalogo e metodos para biblioteca

--- src/App.java
+++ src/App.java
@@ -1,17 +1,19 @@
 // PARA COMPILAR: javac -d bin -sourcepath src src/App.java
 // PARA EXECUTAR: java -cp bin App
 
+import java.util.Scanner;
 import modelo.livro.*;
 
 public class App {
 
     public static void main(String[] args)
      {
+        Scanner sc = new Scanner(System.in);
         
+        Cadastro cadastro = new Cadastro();
         Biblioteca biblioteca = new Biblioteca();
 
-        Livro novoCadastro = biblioteca.cadastrarLivro();
+        Livro novoCadastro = cadastro.cadastrarLivro(sc);
         biblioteca.exibirLivro(novoCadastro);
-        
     }
 }
--- src/modelo/livro/Biblioteca.java
+++ src/modelo/livro/Biblioteca.java
@@ -1,71 +1,29 @@
 package modelo.livro;
 
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 import modelo.FuncoesFormatacao;
+
 
 public class Biblioteca {
 
-    public Livro cadastrarLivro() {
+    private Map<String, Livro> catalogo;
 
-        Livro livro;
-        Scanner sc = new Scanner(System.in);
 
-        FuncoesFormatacao.exibirCabecalho("CADASTRO DE NOVO LIVRO");
-
-        FuncoesFormatacao.solicitarCampo("Título");
-        String titulo = sc.nextLine();
-
-        FuncoesFormatacao.solicitarCampo("Autor");
-        String autor = sc.nextLine();
-
-        FuncoesFormatacao.solicitarCampo("Sinopse");
-        String sinopse = sc.nextLine();
-
-        FuncoesFormatacao.exibirSubtitulo("TIPO DE LIVRO");
-        System.out.println(" 1. Livro Físico");
-        System.out.println(" 2. Ebook");
-        System.out.println(" 3. AudioBook");
-        FuncoesFormatacao.inserirLinha();
-
-        FuncoesFormatacao.solicitarCampo("Escolha uma opção");
-        int op = sc.nextInt();
-        sc.nextLine();
-
-        FuncoesFormatacao.inserirLinha();
-
-        switch (op) {
-            case 1 -> {
-                FuncoesFormatacao.solicitarCampo("Quantidade de páginas");
-                String quantidadePaginas = sc.nextLine();
-                livro = new LivroFisico(titulo, autor, sinopse, 1, quantidadePaginas);
-            }
-
-            case 2 -> {
-                FuncoesFormatacao.solicitarCampo("Quantidade de palavras");
-                String quantidadePalavras = sc.nextLine();
-                livro = new Ebook(titulo, autor, sinopse, 1, quantidadePalavras);
-            }
-
-            case 3 -> {
-                FuncoesFormatacao.solicitarCampo("Duração em minutos");
-                String duracaoMinutos = sc.nextLine();
-                livro = new AudioBook(titulo, autor, sinopse, 1, duracaoMinutos);
-            }
-
-            default -> {
-                sc.close();
-                FuncoesFormatacao.inserirDivisor();
-                throw new IllegalArgumentException("Opção inválida: " + op);
-            }
-        }
-        sc.close();
-
-        FuncoesFormatacao.inserirDivisor();
-        System.out.println(" Livro cadastrado com sucesso!");
-        FuncoesFormatacao.inserirDivisor();
-
-        return livro;
+    public Biblioteca() {
+        this.catalogo = new HashMap<>();
     }
+
+    
+    public void adicionarLivro(Livro livro) {
+        catalogo.put(livro.getTitulo(), livro);
+    }
+
+
+    public void removerLivro(Livro livro) {
+        catalogo.remove(livro.getTitulo());
+    }
+
 
     public void exibirLivro(Livro livro) {
 
